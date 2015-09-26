4e2a2c83e829b70b2f11fdd398ef94c3d24b3444
Bryan Alexander <bryanalexander@me.com>
2015-09-26 10:20:10 -1000

    defined DNA array

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -229,6 +229,7 @@ var linuxFlavors = ['Gentoo', 'Fedora', 'Debian', 'Slackware', 'Red Hat', 'Biebe
  *
  */
 
+ var DNA = ['nucleatides', 'guanine', 'adenine', 'thymine', 'cytosine'];
 
 /* Step 16
  *
