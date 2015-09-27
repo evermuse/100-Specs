6068b52ab85bad8daa7fe8be860cab1dedaf4032
Bryan Alexander <bryanalexander@me.com>
2015-09-26 16:42:00 -1000

    created isWarmBlooded method

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1365,6 +1365,24 @@ var dinner = new Meal(['fish', 'vegetables']);
  *
  */
 
+Animal.prototype.isWarmBlooded = function() {
+
+if (this.species === 'Monkey' || this.species === 'Bird') {
+
+  return true;
+
+}
+
+if (this.species === 'Fish') {
+
+  return false;
+
+}
+
+return 'Could not determine if warm-blooded';
+
+};
+
 
 /* Step 82
  *
