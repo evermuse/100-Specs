4dc9de29825b85eb482c0ce99cd4d61a2227b0e3
Bryan Alexander <bryanalexander@me.com>
2015-09-26 18:29:18 -1000

    assigned Shape instance method results

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1645,8 +1645,9 @@ var forwardDriving = forte.drive();
  * and assign the values to each variable below.
  *
  */
-var decagon;
-var polygon;
+
+var decagon = new Shape(10).getType();
+var polygon = new Shape(16).getType();
 
 
 /* Step 94
